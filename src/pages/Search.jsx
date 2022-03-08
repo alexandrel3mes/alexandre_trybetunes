12bc82d3ceacd958e070e1ff7b6150047cb2da23
Alexandre <alexandreeduardolemes@gmail.com>
2022-03-08 06:35:21 -0300

    Metade do Requisito 6 feita

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import LoadingScreen from '../LoadingScreen';
 
 class Search extends React.Component {
   constructor() {
@@ -8,8 +10,10 @@ class Search extends React.Component {
     this.state = {
       artistName: '',
       SaveButtonDisabled: true,
-      /* loading: false,
-      saved: false, */
+      loading: false,
+      artistAfterFetch: '',
+      artistListLength: 0,
+      clicked: false,
     };
   }
 
@@ -25,39 +29,63 @@ class Search extends React.Component {
     });
   }
 
-  saveBtn = () => {
+  saveBtn = async () => {
     const { artistName } = this.state;
-    console.log(artistName);
+    this.setState({ loading: true });
+    const search = await searchAlbumsAPI(artistName);
+    const arrLength = search.length;
+    this.setState({ artistListLength: arrLength });
+    this.setState({ artistAfterFetch: artistName });
+    this.setState({ artistName: '' });
+    this.setState({ loading: false });
+    this.setState({ clicked: true });
+    console.log(search);
   }
 
   render() {
     const {
       artistName,
       SaveButtonDisabled,
-      /* loading,
-      saved, */
+      loading,
+      artistListLength,
+      artistAfterFetch,
+      clicked,
     } = this.state;
+    const req1 = artistListLength === 0 && clicked;
+    const req2 = artistListLength > 0 && artistAfterFetch.length > 0;
     return (
       <>
         <Header />
         <div data-testid="page-search">
-          <form>
-            <input
-              type="text"
-              data-testid="search-artist-input"
-              placeholder="Nome do Artista"
-              value={ artistName }
-              onChange={ this.handleChange }
-            />
-            <button
-              type="button"
-              disabled={ SaveButtonDisabled }
-              onClick={ this.saveBtn }
-              data-testid="search-artist-button"
-            >
-              Pesquisar
-            </button>
-          </form>
+          {
+            loading ? <LoadingScreen /> : (
+              <form>
+                <input
+                  type="text"
+                  data-testid="search-artist-input"
+                  placeholder="Nome do Artista"
+                  value={ artistName }
+                  onChange={ this.handleChange }
+                />
+                <button
+                  type="button"
+                  disabled={ SaveButtonDisabled }
+                  onClick={ this.saveBtn }
+                  data-testid="search-artist-button"
+                >
+                  Pesquisar
+                </button>
+              </form>
+            )
+          }
+          {
+            req1 && <h3>Nenhum álbum foi encontrado</h3>
+          }
+          {
+            req2 && (
+              <h3>{`Resultado de álbuns de: ${artistAfterFetch}`}</h3>
+            )
+          }
         </div>
       </>
     );
