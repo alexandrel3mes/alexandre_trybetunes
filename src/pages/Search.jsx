6f1b540b9adbad8711dcf40edfb2adcb7e22c52a
Alexandre <alexandreeduardolemes@gmail.com>
2022-03-08 05:39:17 -0300

    Requisito 5 feito

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,63 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      artistName: '',
+      SaveButtonDisabled: true,
+      /* loading: false,
+      saved: false, */
+    };
+  }
+
+  handleChange = ({ target: { value } }) => {
+    const minLength = 2;
+    this.setState({
+      artistName: value,
+    }, () => {
+      this.setState((prevState) => ({
+        SaveButtonDisabled:
+        prevState.artistName.length >= minLength ? !prevState : prevState,
+      }));
+    });
+  }
+
+  saveBtn = () => {
+    const { artistName } = this.state;
+    console.log(artistName);
+  }
+
   render() {
+    const {
+      artistName,
+      SaveButtonDisabled,
+      /* loading,
+      saved, */
+    } = this.state;
     return (
       <>
         <Header />
-        <div data-testid="page-search" />
+        <div data-testid="page-search">
+          <form>
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              placeholder="Nome do Artista"
+              value={ artistName }
+              onChange={ this.handleChange }
+            />
+            <button
+              type="button"
+              disabled={ SaveButtonDisabled }
+              onClick={ this.saveBtn }
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </>
     );
   }
